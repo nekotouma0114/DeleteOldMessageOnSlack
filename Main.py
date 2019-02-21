09b2eb4ee306e50a078751c03fdb21e029a049f2
nekotouma0114 <nekotouma0114@gmail.com>
2019-02-21 23:10:01 +0900

    #1 - 各パラメータをiniに分離

--- Main.py
+++ Main.py
@@ -1,10 +1,35 @@
+import configparser
 
 import Slack;
 
 def main():
-    slack = Slack.Slack()
-    slack.debug()
+    slack_params = read_config("./setting.ini")
+    slack = Slack.Slack(slack_params)
+    slack.print_messages()
 
+#
+#   @params
+#   @return
+#
+def read_config(file_path):
+    ini_file = configparser.SafeConfigParser()
+    ini_file.read(file_path)
+    general_info = dict()
+    try:
+        #Workspaceの共通情報を読み込む
+        general_info["delete_api"]  = ini_file.get("General","delete_api")
+        general_info["history_api"] = ini_file.get("General","history_api")
+        general_info["token"] = ini_file.get("General","token")
+        general_info["workspace"] = ini_file.get("General","workspace")
+
+        #channel情報を読み込む。複数channelに対応できる様になんとかする予定
+        general_info["channel_id"] = ini_file.get("channel1","id")
+    except:
+        #exceptが発生したパラメータを判別できる様になんとかする予定
+        print ("any keywork is not found")
+        return null;
+
+    return general_info
 
 if __name__ == '__main__':
     main();
--- Slack.py
+++ Slack.py
@@ -2,20 +2,26 @@ import requests
 import urllib,urllib.request
 
 class Slack:
-    def __init__(self):
-        self.delete_api_url="https://slack.com/api/chat.delete"
-        self.history_api_url= "https://slack.com/api/channels.history"
-        self.slack_token=""
-        self.slack_channel=""
-        workspace_url="alice-f-cat.slack.com"
+    def __init__(self,general_info,id=None):
+        self.delete_api=general_info['delete_api']
+        self.history_api= general_info['history_api']
+        self.token=general_info['token']
+        self.workspace=general_info['workspace']
+        if id == None :
+            self.channel_id=general_info['channel_id']
+        else:
+            self.channel_id = id
 
 
-    def debug(self):
+    def channel_id(self):
+        self._channel_id = id
+
+    def print_messages(self):
         channel_parm ={
-            "token"     : self.slack_token,
-            "channel"   : self.slack_channel
+            "token"     : self.token,
+            "channel"   : self.channel_id
         }
-        history_request = requests.get(self.history_api_url,params=channel_parm)
+        history_request = requests.get(self.history_api,params=channel_parm)
         history_data = history_request.json()
         if history_data['ok']:
             history_message = history_data['messages']
